4c5f44833ffc7873a2102ab6d286a6d825bbe7c6
Mirco Bertelli <mircobe87@gmail.com>
2014-10-01 15:35:37 +0200

    aggiunta opzione -h a lcd-function-set.sh

--- lcd-function-set.sh
+++ lcd-function-set.sh
@@ -2,12 +2,23 @@
 
 function help () {
 	echo -e "$1\n"
-	echo -e "USO:\n  $0 -l <N_LINES> -c <CHAR_SET>\n"
-	echo -e "OPZIONI:\n  -l  configura il numero di linee; N_LINES può essere 1 o 2."
+	echo -e "Configura il display.\n"
+	echo -e "USO:"
+	echo -e "  $0 -h"
+	echo -e "  $0 -l <N_LINES> -c <CHAR_SET>\n"
+	echo -e "OPZIONI:"
+	echo -e "  -h  mostra questa guida."
+	echo -e "  -l  configura il numero di linee; N_LINES può essere 1 o 2."
 	echo -e "  -c  configura il charset dei caratteri; CHAR_SET può essere 5x8 o 5x10.\n"
-	echo -e "NOTE:\n  lo script utilizza la variabile d'ambiente I2C_BUS per stabilire su quale"
+	echo -e "NOTE:"
+	echo -e "  lo script utilizza la variabile d'ambiente I2C_BUS per stabilire su quale"
 	echo -e "  bus lavorare. Assicurarsi che la variabile sia definita correttamente.\n"
 }
+
+if [ "$1" = "-h" ]; then
+	help
+	exit 0
+fi
 
 if [ -z $I2C_BUS ]; then
 	help "La variabile d'ambiente I2C_BUS non è definita."
@@ -72,18 +83,7 @@ elif [ "$1" = "-l" -a "$2" = "2" -a "$3" = "-c" -a "$4" = "5x10" ]; then
 else
 	# errore nei parametri
 	help "Errore nei parametri"
+	exit 1
 fi
-
-# display on, cursore on + blink
-#i2cset -y 1 0x27 0x00 0x0C
-#i2cset -y 1 0x27 0x00 0x08
-#i2cset -y 1 0x27 0x00 0xFC
-#i2cset -y 1 0x27 0x00 0xF8
-
-# no shift display, shift cursore
-#i2cset -y 1 0x27 0x00 0x0C
-#i2cset -y 1 0x27 0x00 0x08
-#i2cset -y 1 0x27 0x00 0x6C
-#i2cset -y 1 0x27 0x00 0x68
 
 exit 0
