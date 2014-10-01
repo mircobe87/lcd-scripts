30ddf326ad07d9f68f4549c4d2b77ba46c516c28
Mirco Bertelli <mircobe87@gmail.com>
2014-10-01 15:38:03 +0200

    modificato lcd-display-ctrl.sh per usare la variabile d'ambiente I2C_BUS

--- lcd-display-ctrl.sh
+++ lcd-display-ctrl.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
-I2C_BUS="1"
+function help () {
+	echo -e "$1\n"
+	echo -e "Accende o spegne il display, il cursore e il cursore lampeggiante.\n"
+	echo -e "USO:"
+	echo -e "  $0 -h"
+	echo -e "  $0 -d <ON/OFF> -c <ON/OFF> -b <ON/OFF>\n"
+	echo -e "OPZIONI:"
+	echo -e "  -h  mostra questa guida"
+	echo -e "  -d  accende o spegne il display."
+	echo -e "  -c  accende o spegne il cursore."
+	echo -e "  -b  accende o spegne il lampeggiare del cursore.\n"
+	echo -e "  <ON/OFF>  0 - spento"
+	echo -e "            1 - acceso\n"
+	echo -e "NOTE:\n  lo script utilizza la variabile d'ambiente I2C_BUS per stabilire su quale"
+	echo -e "  bus lavorare. Assicurarsi che la variabile sia definita correttamente.\n"
+}
+
+if [ "$1" = "-h" ]; then
+	help
+	exit 0
+fi
+
+if [ -z $I2C_BUS ]; then
+	help "La variabile d'ambiente I2C_BUS non è definita."
+	exit 1
+fi
 
 LCD_ADDR="0x27"
 REG_ADDR="0x00"
@@ -103,14 +128,8 @@ elif [ "$1" = "-d" -a "$2" = "1" -a "$3" = "-c" -a "$4" = "1" -a "$5" = "-b" -a 
 	echo "blink:   ON"
 else
 	# errore parametri
-	echo -e "USO:"
-	echo -e "  $0 -d <ON/OFF> -c <ON/OFF> -b <ON/OFF>\n"
-	echo -e "OPZIONI:"
-	echo -e "  -d  accende o spegne il display."
-	echo -e "  -c  accende o spegne il cursore."
-	echo -e "  -b  accende o spegne il lampeggiare del cursore.\n"
-	echo -e "  <ON/OFF>  0 - spento"
-	echo -e "            1 - acceso\n"
+	help "Errore parametri"
+	exit 1
 fi
 
 exit 0
